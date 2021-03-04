60a0cdf67f6d01b55573cc302f687ca03936f293
Charles Butler <cdbutle@outlook.com>
2021-03-04 13:29:32 -0600

    added events / root reducer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 //react-redux, redux, thunk libraries
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'; //window._Redux_devtools replacement
-import reactDom from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension"; //window._Redux_devtools replacement
 
-import App from './components/App'
+import App from "./components/App";
 
-//need to create a store for redux => createStore 1. reducer 2. dev tools 
+//need to create a store for redux => createStore 1. reducer 2. dev tools
 
-const store = createStore{
-    rootReducer, 
-    composeWithDevTools(applyMiddleware(thunk))
-}
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-reactDom.render{
-    <Provider store={store}>
-        <App />
-
-    </Provider>
-    document.getElementById('root')
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import { eventsReducer } from "./eventsReducer";
+
+export const rootReducer = combineReducers({
+  events: eventsReducer,
+});
