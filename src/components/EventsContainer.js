7e49320d18b01ac5e6f3e1c0815079768fe08f27
Charles Butler <cdbutle@outlook.com>
2021-03-04 15:26:02 -0600

    started events container

---
+++ src/components/EventsContainer.js
@@ -0,0 +1,11 @@
+// contain methods + data for events of the timeline
+
+import React, { Component } from "react";
+
+class EventsContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default EventsContainer;
