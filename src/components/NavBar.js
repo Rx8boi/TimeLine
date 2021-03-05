80484d1c8205ad4861e79a6928dfc9e36ce16590
Charles Butler <cdbutle@outlook.com>
2021-03-05 16:18:27 -0600

    created timeline nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,37 @@ import styled from "styled-components";
 
 const Styles = styled.div`
   .navbar {
-    background-color: #00324;
+    background-color: #222;
+  }
+
+  .navbar-background-color,
+  .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
   }
 `;
+
+export const NavBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">TimeLine</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link href="/intro">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/events">TimeLine</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+);
