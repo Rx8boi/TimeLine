d9e1928b5a494e32e7e77c0bede090f1c484f1aa
Charles Butler <cdbutle@outlook.com>
2021-03-06 13:04:17 -0600

    finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,38 @@
 import React from "react";
+import Developer from "../components/lottie/Developer";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const About = () => {
   return (
     <div>
-      <br></br>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur magni
-        quas maiores sapiente eos soluta voluptatibus nobis perspiciatis omnis
-        debitis necessitatibus corrupti, expedita illo hic dolorum quia atque.
-        Impedit, praesentium.
-      </p>
+      <Container>
+        <Row>
+          <Col>
+            <Developer />
+          </Col>
+          <Col>
+            <h1>About Page</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              magni quas maiores sapiente eos soluta voluptatibus nobis
+              perspiciatis omnis debitis necessitatibus corrupti, expedita illo
+              hic dolorum quia atque. Impedit, praesentium.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              magni quas maiores sapiente eos soluta voluptatibus nobis
+              perspiciatis omnis debitis necessitatibus corrupti, expedita illo
+              hic dolorum quia atque. Impedit, praesentium. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Pariatur magni quas maiores
+              sapiente eos soluta voluptatibus nobis perspiciatis omnis debitis
+              necessitatibus corrupti, expedita illo hic dolorum quia atque.
+              Impedit, praesentium.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
