6f286a5ddc294ac9f3bb724e49ec3d9e3c9f8109
Charles Butler <cdbutle@outlook.com>
2021-03-08 12:35:55 -0600

    events styling

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -25,7 +25,22 @@ const Styles = styled.div`
   }
 
   #description {
-    margin: 1.2em 0 2em 0;
+    margin: 0.9em 0 1em 0;
+  }
+
+  .subtexts {
+    background-color: white;
+    margin: 10px auto;
+    width: 260px;
+    box-shadow: 0 0.34em 0.5em 0 rgba(16, 24, 60, 0.25),
+      0 0.5em 1.25em 0 rgba(61, 23, 79, 0.15) !important;
+    position: relative;
+    border-radius: 15px;
+    height: 130px;
+    display: inline-block;
+    padding: 14px;
+    text-align: left;
+    font-family: "Montserrat", sans-serif;
   }
 `;
 
@@ -33,10 +48,11 @@ const EventsList = ({ events }) => {
   return (
     <div>
       <Styles>
+        <br></br>
         <VerticalTimeline>
           {events.map((event) => (
             <VerticalTimelineElement
-              contentStyle={{ background: "#e6e6e6", color: "black" }}
+              contentStyle={{ background: "#f2f2f2", color: "black" }}
               contentArrowStyle={{ borderRight: "9px solid  black" }}
               key={event.id}
               date={event.date}
@@ -48,10 +64,12 @@ const EventsList = ({ events }) => {
               icon={<TimeIcon />}
             >
               <h3 className="vertical-timeline-element-title">{event.title}</h3>
-              <sub className="vertical-timeline-element-subtitle">
-                {event.event_type}
-              </sub>
-              <p id="description">{event.summary}</p>
+              <div className="subtexts">
+                <sub className="vertical-timeline-element-subtitle">
+                  {event.event_type}
+                </sub>
+                <p id="description">{event.summary}</p>
+              </div>
             </VerticalTimelineElement>
           ))}
         </VerticalTimeline>
