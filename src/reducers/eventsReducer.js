143464a63bbb582030765575ca41690438b708a9
Charles Butler <cdbutle@outlook.com>
2021-03-08 12:41:59 -0600

    adding delete function

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -4,6 +4,9 @@ export const eventsReducer = (state = [], action) => {
       return action.payload;
     case "ADD_EVENT":
       return [...state, action.payload];
+    case "REMOVE_EVENT":
+      let newEvents = state.filter((event) => event.id !== action.payload);
+      return [...newEvents];
     default:
       return state;
   }
