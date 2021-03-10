3be7773f9f72b1e33d5d305e23775e9924eb15ea
Charles Butler <cdbutle@outlook.com>
2021-03-10 17:51:56 -0600

    added tooltip

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -9,10 +9,13 @@ import "react-vertical-timeline-component/style.min.css";
 import { ReactComponent as TimeIcon } from "../assets/icons8-automation.svg";
 import { ReactComponent as AddIcon } from "../assets/icons8-plus.svg";
 import styled from "styled-components";
-import { Button, ModalBody } from "react-bootstrap";
+import { ModalBody } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import EventsForm from "./EventsForm";
 import { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import { BsFillBackspaceFill } from "react-icons/bs";
+import { FcTimeline } from "react-icons/fc";
 
 const Styles = styled.div`
   .add {
@@ -65,11 +68,11 @@ const Styles = styled.div`
   }
 
   .buttondark {
+    cursor: pointer;
     color: black;
-    font-family: "Montserrat", sans-serif;
     position: absolute;
-    top: 1px;
-    left: 410px;
+    top: 0px;
+    left: 9px;
     padding: 0px;
     margin: 1px;
   }
@@ -79,69 +82,77 @@ const EventsList = ({ events, removeEvent }) => {
   const [show, setShow] = useState(false);
 
   return (
-    <div>
-      <Styles>
-        <br></br>
-        <VerticalTimeline>
-          {events.map((event) => (
-            <VerticalTimelineElement
-              contentStyle={{ background: "#f2f2f2", color: "black" }}
-              contentArrowStyle={{ borderRight: "9px solid  black" }}
-              key={event.id}
-              date={event.date}
-              dateClassName="date"
-              iconStyle={{
-                background: "black",
-                opacity: ".9",
-              }}
-              icon={<TimeIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">{event.title}</h3>
-              <span className="subtexts">
-                <sub className="vertical-timeline-element-subtitle">
-                  {event.event_type}
-                </sub>
-                <p id="description">{event.summary}</p>
-              </span>
-              <Button
-                variant="link"
-                className="buttondark"
-                onClick={() => removeEvent(event)}
-              >
-                x
-              </Button>
-            </VerticalTimelineElement>
-          ))}
-          <span variant="link" className="add" onClick={() => setShow(true)}>
-            <VerticalTimelineElement
-              icon={<AddIcon />}
-              contentStyle={{ opacity: "0", display: "none" }}
-              box-shadow={{ opacity: "0" }}
-              contentArrowStyle={{ borderRight: "10px solid  black" }}
-              iconStyle={{
-                background: "black",
-                opacity: ".9",
-              }}
-            ></VerticalTimelineElement>
-          </span>
-          <Modal
-            show={show}
-            onHide={() => setShow(false)}
-            dialogClassName="modal-90w"
-            aria-labelledby="example-custom-modal-styling-title"
+    <Styles>
+      <br></br>
+      <VerticalTimeline>
+        <ReactTooltip />
+        {events.map((event) => (
+          <VerticalTimelineElement
+            contentStyle={{ background: "#f2f2f2", color: "black" }}
+            contentArrowStyle={{ borderRight: "9px solid  black" }}
+            key={event.id}
+            date={event.date}
+            dateClassName="date"
+            iconStyle={{
+              background: "black",
+              opacity: ".9",
+            }}
+            icon={<TimeIcon />}
           >
-            <Modal.Header closeButton>
-              <Modal.Title id="example-custom-modal-styling-title">
+            <h3 className="vertical-timeline-element-title">{event.title}</h3>
+            <span className="subtexts">
+              <sub className="vertical-timeline-element-subtitle">
+                {event.event_type}
+              </sub>
+              <p id="description">{event.summary}</p>
+            </span>
+            <span
+              variant="link"
+              className="buttondark"
+              onClick={() => removeEvent(event)}
+            >
+              <BsFillBackspaceFill />
+            </span>
+          </VerticalTimelineElement>
+        ))}
+        <span
+          variant="link"
+          className="add"
+          data-tip="Click to add"
+          data-place="bottom"
+          onClick={() => setShow(true)}
+        >
+          <VerticalTimelineElement
+            icon={<AddIcon />}
+            contentStyle={{ opacity: "0", display: "none" }}
+            box-shadow={{ opacity: "0" }}
+            contentArrowStyle={{ borderRight: "10px solid  black" }}
+            iconStyle={{
+              background: "black",
+              opacity: ".9",
+            }}
+          ></VerticalTimelineElement>
+        </span>
+        <Modal
+          show={show}
+          onHide={() => setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-custom-modal-styling-title">
+              <h2>
+                <FcTimeline />
                 Add Event Modal
-              </Modal.Title>
-            </Modal.Header>
-            <ModalBody>
-              <EventsForm />
-            </ModalBody>
-          </Modal>
-        </VerticalTimeline>
-      </Styles>
-    </div>
+              </h2>
+            </Modal.Title>
+          </Modal.Header>
+          <ModalBody>
+            <EventsForm />
+          </ModalBody>
+        </Modal>
+      </VerticalTimeline>
+    </Styles>
   );
 };
 
