3bfc5ac2e4196e7965835fdef3c624c3b08bfb65
Charles Butler <cdbutle@outlook.com>
2021-07-22 21:00:00 -0500

    comment deletion

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import App from "./components/App";
 import { rootReducer } from "./reducers/rootReducer";
 
 //need to create a store for redux => createStore 1. reducer 2. dev tools
-
+//creating store & also saying what we're doing to update (reducer and how we're intercepting / thunk)
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
